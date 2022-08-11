3925f8cf619fd23e19ef685835fa991583244ae3
Jonathan Wang <arachnophobicspyder@gmail.com>
2022-08-11 13:23:21 +1000

    Got 3 states working

--- Assets/Scripts/AiAgent.cs
+++ Assets/Scripts/AiAgent.cs
@@ -5,28 +5,68 @@ using UnityEngine;
 public class AiAgent : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
+    private float _moveSpeed;
     [SerializeField] private GameObject _player;
+    private Vector3 _lastSeen;
 
     [SerializeField] private Transform[] _waypoints;
     [SerializeField] private int _index;
-    void Update()
+    [SerializeField] private float _viewRange=5f;
+    public bool IsPlayerInRange()
     {
+        //Ternary operator "variable = (condition) ? expressionTrue :  expressionFalse;"
+        bool _inRange = (Vector2.Distance(transform.position, _player.transform.position)<_viewRange) ? true : false;
+        return _inRange;
+    }
+    
+    public void ChasePlayer()
+    {
+        _moveSpeed = _speed * 1.2f;
+        MoveToPoint(_player.transform.position);
+        _lastSeen = _player.transform.position;
+    }
+
+    public void HuntForPlayer()
+    {
+        MoveToPoint(_lastSeen);
+
+        //dumb orbit code
+        /*if (Vector2.Distance(transform.position, _lastSeen) < 1)
+        {
+            transform.RotateAround(_lastSeen,Vector3.up,_moveSpeed*Time.deltaTime);
+        }*/
+    }
+
+    public void Patrol()
+    {
+        _moveSpeed = _speed * 0.8f;
         MoveToPoint(_waypoints[_index].position);
 
-        if (Vector2.Distance(transform.position, _waypoints[_index].position)<1)
+        if (Vector2.Distance(transform.position, _waypoints[_index].position) < 1)
         {
-            _index = (_index+1)%(_waypoints.Length);
+            _index = (_index + 1) % (_waypoints.Length);
         }
-        //MoveToPoint(_player.transform.position);
     }
+
     void MoveToPoint(Vector2 target)
     {
         Vector2 directionToPlayer = target - (Vector2)transform.position;
         if (directionToPlayer.magnitude > 0.1f)
         {
             directionToPlayer.Normalize();
-            directionToPlayer *= _speed * Time.deltaTime;
+            directionToPlayer *= _moveSpeed * Time.deltaTime;
             transform.position += (Vector3)directionToPlayer;
+        }
+    }
+
+    public void Search()
+    {
+        for (int i = 0; i < _waypoints.Length; i++)
+        {
+            if (Vector2.Distance(transform.position, _waypoints[_index].position) > Vector2.Distance(transform.position, _waypoints[i].position))
+            {
+                _index = i;
+            }
         }
     }
 }
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AiAgent))]
 public class StateMachine : MonoBehaviour
 {
     public enum State
     {
         Patrol, 
         Chase, 
-        Search,
+        Hunt,
     }
 
     [SerializeField] private State _state;
+    [SerializeField] private float _huntTime = 5f;
+
+    private AiAgent _aiAgent;
+    private SpriteRenderer _spriteRenderer;
     private void Start()
     {
+        _aiAgent = GetComponent<AiAgent>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
         NextState();
     }
     private void NextState()
@@ -24,11 +32,11 @@ public class StateMachine : MonoBehaviour
                 StartCoroutine(PatrolState());
                 break;
             case State.Chase:
-                StartCoroutine(AlertState());
+                StartCoroutine(ChaseState());
                 break;
-            /*case State.Search:
-                StartCoroutine(SearchState());
-                break;*/
+            case State.Hunt:
+                StartCoroutine(HuntState());
+                break;
             default:
                 Debug.Log("404 State not found, State does not exist in NextState function, stoppings statemachine \n_state: " + _state);
                 break;
@@ -36,23 +44,50 @@ public class StateMachine : MonoBehaviour
     }
     private IEnumerator PatrolState()
     {
+        _spriteRenderer.color = Color.yellow;
+        _aiAgent.Search();
         while (_state == State.Patrol) //while loop runs once per game step
         {
             //run this code
+            _aiAgent.Patrol();
+            if (_aiAgent.IsPlayerInRange()) { _state = State.Chase;}
             yield return null;
         }
         Debug.Log("State: Exiting Patrol");
         NextState();
     }
 
-    private IEnumerator AlertState()
+    private IEnumerator ChaseState()
     {
+        _spriteRenderer.color = Color.red;
         while (_state == State.Chase)
         {
             //run this code
+            _aiAgent.ChasePlayer();
+            if (!_aiAgent.IsPlayerInRange()) { _state = State.Hunt; }
             yield return null;
         }
         Debug.Log("State: Exiting Alert");
+        NextState();
+    }
+    private IEnumerator HuntState()
+    {
+        _spriteRenderer.color = Color.magenta;
+        float timer = _huntTime;
+        while (_state == State.Hunt && timer>0)
+        {
+            //run this code
+            _aiAgent.HuntForPlayer();
+            timer -= Time.deltaTime;
+            if (_aiAgent.IsPlayerInRange())
+            {
+                _state = State.Chase;
+                break;
+            }
+            yield return null;
+        }
+        if (!_aiAgent.IsPlayerInRange()) { _state = State.Patrol; }
+        Debug.Log("State: Exiting Hunt");
         NextState();
     }
 }
